f03da8fee8875cb736e30beb5d90c7ff1824f78e
Hallowdread <hallowpahe@gmail.com>
2025-07-23 08:54:27 +0100

    fix: update Leaflet icon paths for deployment

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -14,6 +14,18 @@ import { useUrlPosition } from "../../hooks/useUrlPosition";
 import Button from "../Button/Button";
 import styles from "./Map.module.css";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet's default icon paths for deployment
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function Map() {
   const { cities } = useCities();
